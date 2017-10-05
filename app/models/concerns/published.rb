fb4c2d9b642cae9622c1382a58a968324e2cd1b2
AlexanderShvaykin <skiline.alex@gmail.com>
2017-10-06 00:50:56 +0300

    ref. published

---
+++ app/models/concerns/published.rb
@@ -0,0 +1,11 @@
+module Published
+  extend ActiveSupport::Concern
+
+  included do
+    before_create :set_published_at
+  end
+
+  def set_published_at
+    self.published_at = Time.zone.now
+  end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,9 @@
 class Post < ApplicationRecord
+  include Published
   belongs_to :author, polymorphic: true
+  has_many :comments, as: 'commentable', dependent: :destroy
 
   validates :title, :body, presence: true
 
   before_create :set_published_at
-
-  private
-
-  def set_published_at
-    self.published_at = Time.zone.now
-  end
 end
