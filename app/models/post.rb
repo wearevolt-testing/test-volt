15b02eafcb7503cd4b3d24953df9b4b7700d3ac5
AlexanderShvaykin <skiline.alex@gmail.com>
2017-10-06 00:13:31 +0300

    add published_at callback

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :author, polymorphic: true
 
   validates :title, :body, presence: true
+
+  before_create :set_published_at
+
+  private
+
+  def set_published_at
+    self.published_at = Time.zone.now
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -6,4 +6,15 @@ RSpec.describe Post, type: :model do
     it { is_expected.to validate_presence_of(:body) }
     it { is_expected.to belong_to(:author) }
   end
+
+  context 'callbacks' do
+    let(:author) { create(:user) }
+    let(:post) { create(:post, author: author) }
+
+    it 'published_at auto save after create post' do
+      reload_post = Post.find post.id
+      post = reload_post
+      expect(post.reload.published_at.to_s.first(-9)).to eq post.created_at.to_s.first(-9)
+    end
+  end
 end
