c1fe0ad3e884e7d5eabbc95f3174bbc66043b564
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2017-10-06 09:24:30 +0300

    add user nickname validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,10 @@
+require 'rails_helper'
+
 describe User do
   let(:user) { described_class.new(email: 'user@example.com') }
 
   it { is_expected.to respond_to(:email) }
+  it { is_expected.to validate_presence_of(:nickname) }
 
   it "#email returns a string" do
     expect(user.email).to match 'user@example.com'
