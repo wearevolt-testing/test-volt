be69f63268ca90efe1f081880e41d35c573919f6
AlexanderShvaykin <skiline.alex@gmail.com>
2017-10-10 00:18:14 +0300

    add api post create spec

--- spec/api/v1/posts/create_spec.rb
+++ spec/api/v1/posts/create_spec.rb
@@ -5,9 +5,19 @@ describe 'Posts API::V1 POST /create' do
 
   it_behaves_like 'authorization_doorkeeper'
 
+  context 'valide attributies' do
+    let(:attributies) { attributes_for(:post).merge(access_token: access_token.token) }
+
+    it 'create new post' do
+      expect do post '/api/v1/posts', as: :json, params: { post: attributies,
+                                                           access_token: access_token.token } end
+        .to change(Post, :count).by(1)
+    end
+  end
+
   def do_request(options = {})
     post '/api/v1/posts', as: :json,
-                              params: { post: { title: 'Title', body: 'Body' } }
-                                .merge(options)
+                          params: { post: { title: 'Title', body: 'Body' } }
+                            .merge(options)
   end
 end
