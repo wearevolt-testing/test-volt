e4325b53044616507bffe99787abaf3e9e0afb1a
Alexander Shvaykin <skiline.alex@gmail.com>
2017-10-16 20:53:51 +0300

    add nickname to singup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   use_doorkeeper
   root to: 'visitors#index'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   resources :users
 
   namespace :api do
