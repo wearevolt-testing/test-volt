cc66b0a3307dbb4d462defe9f5a99804ccf4a2f6
Alexander Shvaykin <skiline.alex@gmail.com>
2017-10-16 21:50:13 +0300

    add avatar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171009190746) do
+ActiveRecord::Schema.define(version: 20171016180335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,6 +92,7 @@ ActiveRecord::Schema.define(version: 20171009190746) do
     t.string "name"
     t.string "nickname"
     t.boolean "admin", default: false
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
