877abc8999b60b14db67435774e0ee1ae17a6f64
Alexander Shvaykin <skiline.alex@gmail.com>
2017-10-18 23:35:33 +0300

    add call create report command to report controller

--- app/controllers/api/v1/reports_controller.rb
+++ app/controllers/api/v1/reports_controller.rb
@@ -3,7 +3,9 @@ module Api
     class ReportsController < BaseController
 
       def by_author
-        
+        command = CreateReportService.new(:by_author, self).call
+        status = command.success ? :ok : :bad_request
+        render json: command.message, status: status
       end
     end
   end
