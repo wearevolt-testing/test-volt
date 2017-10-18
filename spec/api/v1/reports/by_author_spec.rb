454e758f2ac135f5439eccfa4a193f87d4c72390
Alexander Shvaykin <skiline.alex@gmail.com>
2017-10-18 23:35:47 +0300

    spec report contr

--- spec/api/v1/reports/by_author_spec.rb
+++ spec/api/v1/reports/by_author_spec.rb
@@ -8,12 +8,17 @@ describe 'Posts API::V1 POST /create' do
   let(:subject) do
     post '/api/v1/reports/by_author', as: :json,
                                       params: {
-                                        start_date: 2.days.ago,
-                                        end_date: Time.zone.now,
+                                        start_date: Date.today.-(3).to_s,
+                                        end_date: Date.today.to_s,
                                         email: 'admin@hero.com',
                                         access_token: access_token
                                       }
   end
 
   it_behaves_like 'authorization_doorkeeper'
+
+  it 'init create_report service' do
+    expect_any_instance_of(CreateReportService).to receive(:call).and_call_original
+    subject
+  end
 end
