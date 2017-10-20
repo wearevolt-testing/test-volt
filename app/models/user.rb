38498131ed8de2c39817f30b540bc888eae29bfe
Alexander Shvaykin <skiline.alex@gmail.com>
2017-10-21 00:50:21 +0300

    add sort method to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   mount_uploader :avatar, AvatarUploader
+
+  def by_author_index(date_range)
+    raise ArgumentError if Date.parse(date_range.first) > Date.parse(date_range.last)
+
+    condition = { published_at: date_range }
+    posts_count = posts.where(condition).count
+    comment_count = comments.where(condition).count
+
+    (posts_count + comment_count) / 10.0
+  end
 end
