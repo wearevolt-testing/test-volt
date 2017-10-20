4de8e639fef86502734b6ee052b1270ae3af2b26
Alexander Shvaykin <skiline.alex@gmail.com>
2017-10-20 23:01:34 +0300

    add sample db seed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,5 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    redirect_to root_path, alert: "Access denied." unless @user == current_user
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,31 @@
 def create_comments(commentable, count = 2)
-  count.times do 
-    text = Faker::Lovecraft.paragraph
+  count.times do
     commentable.comments.create! body: text
-    puts "CREATED COMMETS: #{text}"
   end
 end
 
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
 
-users = [1..5].to_a.map do
-  User.create! email: Faker::Internet.email,
+users = (1..5).to_a.map do
+  user = User.create! email: Faker::Internet.email,
               password: '12345678',
               password_confirmation: '12345678',
               nickname: Faker::StarTrek.character
+  puts 'CREATED USER: ' << user.email
+  user
 end
 
-10.times do |i|
-  post = users.sample.posts.create! title: Faker::StarWars.character, body: Faker::StarWars.quote
-  puts "CREATED POSTS: #{i.next}" if post.valid?
-  create_comments post
+100.times do |i|
+  post = users.sample.posts.create! title: Faker::StarWars.character,
+                                    body: Faker::StarWars.quote,
+                                    published_at: Date.today - rand(100)
+
+  puts 'CREATED POSTS' if post.valid?
+end
+
+Post.all.each do |p|
+  text = Faker::Lovecraft.paragraph
+  User.order("RANDOM()").first.comments.create commentable: p, body: text
+  puts 'CREATED COMMETS'
 end
